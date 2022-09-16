1479f314661406ed56f7f76ed7e5a926dde788cf
Kairo Araujo <kairo@kairo.eti.br>
2022-09-16 16:30:31 +0200

    Add support for SSL
    This commit add the support on ``kaprien-rest-api`` to serve the Rest
    API using HTTPS.
    
    Linked to:
    - https://github.com/kaprien/kaprien/issues/6
    
    Signed-off-by: Kairo Araujo <kairo@kairo.eti.br>

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+if [[ -z ${SECRETS_KAPRIEN_SSL_CERT} ]]; then
+    echo "uvicorn app:kaprien_app --host 0.0.0.0 --port 80"
+else
+    if [[ -z ${SECRETS_KAPRIEN_SSL_KEY} ]]; then
+        echo "Missing variable SECRETS_KAPRIEN_SSL_KEY"
+        exit 1
+    fi
+    echo "uvicorn app:kaprien_app --host 0.0.0.0 --port 443 --ssl-keyfile ${SECRETS_KAPRIEN_SSL_KEY} --ssl-certfile ${SECRETS_KAPRIEN_SSL_CERT}"
+fi
