191d8b919f124118723768a55089ea3c306c6aab
Kairo Araujo <kairo@kairo.eti.br>
2023-01-25 15:57:27 +0100

    Add bandit pre commit (#235)
    * Add bandit to the pre-commit
    
    In order to achive the OpenSSF Best Practices badge we need to implement
    the static code analysis tool.
    
    This commit adds the `bandit` to the RSTUF API.
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    * fix bandit findings
    
    - ignore false positive hard coded token/password
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    * Added comment to bandit false/positive alert
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- repository_service_tuf_api/__init__.py
+++ repository_service_tuf_api/__init__.py
@@ -31,10 +31,10 @@ class SCOPES_NAMES(Enum):
     read_bootstrap = "read:bootstrap"
     read_settings = "read:settings"
     read_tasks = "read:tasks"
-    read_token = "read:token"
+    read_token = "read:token"  # nosec bandit: not hard coded password
     write_bootstrap = "write:bootstrap"
     write_targets = "write:targets"
-    write_token = "write:token"
+    write_token = "write:token"  # nosec bandit: not hard coded password
     delete_targets = "delete:targets"
 
 
