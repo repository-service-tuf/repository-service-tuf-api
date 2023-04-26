33c73bb87e8703cf8959bc588b5b85255f57618b
Kairo de Araujo <kdearaujo@vmware.com>
2023-04-26 17:37:12 +0200

    fix linting for UT on `test__init__.py`
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- tests/unit/tuf_repository_service_api/test__init__.py
+++ tests/unit/tuf_repository_service_api/test__init__.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 import pretend
+
 import repository_service_tuf_api
 
 
@@ -9,7 +10,7 @@ class TestInit:
     def test_is_bootstrap_done_none(self):
         repository_service_tuf_api.settings_repository = pretend.stub(
             reload=pretend.call_recorder(lambda: None),
-            get_fresh=pretend.call_recorder(lambda *a: None)
+            get_fresh=pretend.call_recorder(lambda *a: None),
         )
 
         result = repository_service_tuf_api.is_bootstrap_done()
@@ -18,10 +19,8 @@ class TestInit:
     def test_is_bootstrap_done_with_task_id(self):
         repository_service_tuf_api.settings_repository = pretend.stub(
             reload=pretend.call_recorder(lambda: None),
-            get_fresh=pretend.call_recorder(lambda *a: "taskid")
+            get_fresh=pretend.call_recorder(lambda *a: "taskid"),
         )
 
         result = repository_service_tuf_api.is_bootstrap_done()
         assert result is True
-
-
