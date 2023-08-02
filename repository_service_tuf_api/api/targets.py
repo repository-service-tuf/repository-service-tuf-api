1a553c26487c3d533054a59e1ef0cf44a1741d99
Kairo Araujo <kdearaujo@vmware.com>
2023-08-02 18:38:02 +0200

    Add a simple alias router to /api/v1/artifacts (#395)
    This was a request/feedback received.
    
    As the `targets` might sound confusing on the documentation and TUF
    specification, the term `artifacts` reduces the confusion between
    the target files and the target roles.
    
    Also, in the future, when there is a delegation targets metadata
    feature, it can turn more confusing.
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- repository_service_tuf_api/api/targets.py
+++ repository_service_tuf_api/api/targets.py
@@ -2,15 +2,25 @@
 #
 # SPDX-License-Identifier: MIT
 
-from fastapi import APIRouter, Security, status
+from fastapi import APIRouter, Security, requests, status
 
 from repository_service_tuf_api import SCOPES_NAMES, targets
 from repository_service_tuf_api.api import get_auth
+
+deprecation_warning = (
+    " Deprecation Warning: this endpoint will removed in v1.0.0, "
+    "use `/api/v1/artifacts`"
+)
 
 auth = get_auth()
 
 router = APIRouter(
     prefix="/targets",
+    tags=["v1"],
+    responses={404: {"description": "Not found"}},
+)
+router_alias = APIRouter(
+    prefix="/artifacts",
     tags=["v1"],
     responses={404: {"description": "Not found"}},
 )
@@ -26,12 +36,29 @@ router = APIRouter(
     response_model=targets.Response,
     response_model_exclude_none=True,
     status_code=status.HTTP_202_ACCEPTED,
+    deprecated=True,
+)
+@router_alias.post(
+    "/",
+    summary=(
+        "Add artifacts to Metadata. "
+        f"Scope: {SCOPES_NAMES.write_targets.value}"
+    ),
+    description="Add artifacts to Metadata.",
+    response_model=targets.Response,
+    response_model_exclude_none=True,
+    status_code=status.HTTP_202_ACCEPTED,
 )
 def post(
     payload: targets.AddPayload,
+    request: requests.Request,
     _user=Security(auth, scopes=[SCOPES_NAMES.write_targets.value]),
 ):
-    return targets.post(payload)
+    response = targets.post(payload)
+    if "/api/v1/targets/" in request.url.path:
+        response.message += deprecation_warning
+
+    return response
 
 
 @router.delete(
@@ -44,12 +71,29 @@ def post(
     response_model=targets.Response,
     response_model_exclude_none=True,
     status_code=status.HTTP_202_ACCEPTED,
+    deprecated=True,
+)
+@router_alias.delete(
+    "/",
+    summary=(
+        "Remove artifacts from Metadata. "
+        f"Scope: {SCOPES_NAMES.delete_targets.value}"
+    ),
+    description="Remove artifacts from Metadata.",
+    response_model=targets.Response,
+    response_model_exclude_none=True,
+    status_code=status.HTTP_202_ACCEPTED,
 )
 def delete(
     payload: targets.DeletePayload,
+    request: requests.Request,
     _user=Security(auth, scopes=[SCOPES_NAMES.delete_targets.value]),
 ):
-    return targets.delete(payload)
+    response = targets.delete(payload)
+    if "/api/v1/targets/" in request.url.path:
+        response.message += deprecation_warning
+
+    return response
 
 
 @router.post(
@@ -65,8 +109,28 @@ def delete(
     response_model=targets.Response,
     response_model_exclude_none=True,
     status_code=status.HTTP_202_ACCEPTED,
+    deprecated=True,
+)
+@router_alias.post(
+    "/publish/",
+    summary=(
+        "Submit a task to publish artifacts."
+        f"Scope: {SCOPES_NAMES.write_targets.value}"
+    ),
+    description=(
+        "Trigger a task to publish artifacts not yet published from the "
+        "RSTUF Database"
+    ),
+    response_model=targets.Response,
+    response_model_exclude_none=True,
+    status_code=status.HTTP_202_ACCEPTED,
 )
 def post_publish_targets(
+    request: requests.Request,
     _user=Security(auth, scopes=[SCOPES_NAMES.write_targets.value]),
 ):
-    return targets.post_publish_targets()
+    response = targets.post_publish_targets()
+    if "/api/v1/targets/" in request.url.path:
+        response.message += deprecation_warning
+
+    return response
--- tests/unit/api/test_targets.py
+++ tests/unit/api/test_targets.py
@@ -11,7 +11,7 @@ from fastapi import status
 
 class TestPostTargets:
     def test_post(self, monkeypatch, test_client, token_headers):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         with open("tests/data_examples/targets/payload.json") as f:
             f_data = f.read()
 
@@ -69,7 +69,7 @@ class TestPostTargets:
     def test_post_with_add_task_id_to_custom(
         self, monkeypatch, test_client, token_headers
     ):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         with open("tests/data_examples/targets/payload.json") as f:
             f_data = f.read()
 
@@ -142,7 +142,7 @@ class TestPostTargets:
     def test_post_publish_targets_false(
         self, monkeypatch, test_client, token_headers
     ):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         with open("tests/data_examples/targets/payload.json") as f:
             f_data = f.read()
 
@@ -200,7 +200,7 @@ class TestPostTargets:
     def test_post_without_bootstrap(
         self, monkeypatch, test_client, token_headers
     ):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         with open("tests/data_examples/targets/payload.json") as f:
             f_data = f.read()
 
@@ -216,7 +216,7 @@ class TestPostTargets:
         }
 
     def test_post_missing_required_field(self, test_client, token_headers):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         payload = {
             "targets": [
                 {
@@ -235,7 +235,7 @@ class TestPostTargets:
         headers = {
             "Authorization": "Bearer 123456789abcef",
         }
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         with open("tests/data_examples/targets/payload.json") as f:
             f_data = f.read()
 
@@ -258,7 +258,7 @@ class TestPostTargets:
         headers = {
             "Authorization": f"Bearer {token.json()['access_token']}",
         }
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
         with open("tests/data_examples/targets/payload.json") as f:
             f_data = f.read()
 
@@ -270,10 +270,70 @@ class TestPostTargets:
             "detail": {"error": "scope 'write:targets' not allowed"}
         }
 
+    def test_post_deprecation(self, monkeypatch, test_client, token_headers):
+        url = "/api/v1/targets/"
+        with open("tests/data_examples/targets/payload.json") as f:
+            f_data = f.read()
+
+        payload = json.loads(f_data)
+
+        mocked_repository_metadata = pretend.stub(
+            apply_async=pretend.call_recorder(lambda **kw: None)
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.is_bootstrap_done",
+            lambda: True,
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.repository_metadata",
+            mocked_repository_metadata,
+        )
+        fake_task_id = uuid4().hex
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.get_task_id",
+            lambda: fake_task_id,
+        )
+        fake_time = datetime.datetime(2019, 6, 16, 9, 5, 1)
+        fake_datetime = pretend.stub(
+            now=pretend.call_recorder(lambda: fake_time)
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.datetime", fake_datetime
+        )
+        response = test_client.post(url, json=payload, headers=token_headers)
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        assert response.json() == {
+            "data": {
+                "targets": ["file1.tar.gz", "file2.tar.gz"],
+                "task_id": fake_task_id,
+                "last_update": "2019-06-16T09:05:01",
+            },
+            "message": (
+                "Target(s) successfully submitted. "
+                "Deprecation Warning: this endpoint will removed in v1.0.0, "
+                "use `/api/v1/artifacts`"
+            ),
+        }
+        assert mocked_repository_metadata.apply_async.calls == [
+            pretend.call(
+                kwargs={
+                    "action": "add_targets",
+                    "payload": {
+                        **payload,
+                        "publish_targets": True,
+                        "add_task_id_to_custom": False,
+                    },
+                },
+                task_id=fake_task_id,
+                queue="metadata_repository",
+                acks_late=True,
+            )
+        ]
+
 
 class TestDeleteTargets:
     def test_delete(self, monkeypatch, test_client, token_headers):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
 
         payload = {
             "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"],
@@ -332,7 +392,7 @@ class TestDeleteTargets:
     def test_delete_publish_targets_false(
         self, monkeypatch, test_client, token_headers
     ):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
 
         payload = {
             "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"],
@@ -396,7 +456,7 @@ class TestDeleteTargets:
     def test_delete_without_bootstrap(
         self, monkeypatch, test_client, token_headers
     ):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
 
         payload = {
             "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"]
@@ -416,7 +476,7 @@ class TestDeleteTargets:
         }
 
     def test_delete_missing_required_field(self, test_client, token_headers):
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
 
         payload = {"paths": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"]}
 
@@ -431,7 +491,7 @@ class TestDeleteTargets:
         headers = {
             "Authorization": "Bearer 123456789abcef",
         }
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
 
         payload = {
             "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"]
@@ -447,7 +507,7 @@ class TestDeleteTargets:
             "detail": {"error": "Failed to validate token"}
         }
 
-    def test_post_forbidden_user_incorrect_scope_token(self, test_client):
+    def test_delete_forbidden_user_incorrect_scope_token(self, test_client):
         token_url = "/api/v1/token/?expires=1"
         token_payload = {
             "username": "admin",
@@ -458,7 +518,7 @@ class TestDeleteTargets:
         headers = {
             "Authorization": f"Bearer {token.json()['access_token']}",
         }
-        url = "/api/v1/targets/"
+        url = "/api/v1/artifacts/"
 
         payload = {
             "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"]
@@ -474,10 +534,71 @@ class TestDeleteTargets:
             "detail": {"error": "scope 'delete:targets' not allowed"}
         }
 
+    def test_delete_depreaction(self, monkeypatch, test_client, token_headers):
+        url = "/api/v1/targets/"
+
+        payload = {
+            "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"],
+        }
+
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.is_bootstrap_done",
+            lambda: True,
+        )
+        mocked_repository_metadata = pretend.stub(
+            apply_async=pretend.call_recorder(lambda **kw: None)
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.repository_metadata",
+            mocked_repository_metadata,
+        )
+        fake_task_id = uuid4().hex
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.get_task_id",
+            lambda: fake_task_id,
+        )
+        fake_time = datetime.datetime(2019, 6, 16, 9, 5, 1)
+        fake_datetime = pretend.stub(
+            now=pretend.call_recorder(lambda: fake_time)
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.datetime", fake_datetime
+        )
+
+        # https://github.com/tiangolo/fastapi/issues/5649
+        response = test_client.request(
+            "DELETE", url, json=payload, headers=token_headers
+        )
+
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        assert response.json() == {
+            "data": {
+                "targets": ["file-v1.0.0_i683.tar.gz", "v0.4.1/file.tar.gz"],
+                "task_id": fake_task_id,
+                "last_update": "2019-06-16T09:05:01",
+            },
+            "message": (
+                "Remove Target(s) successfully submitted. "
+                "Deprecation Warning: this endpoint will removed in v1.0.0, "
+                "use `/api/v1/artifacts`"
+            ),
+        }
+        assert mocked_repository_metadata.apply_async.calls == [
+            pretend.call(
+                kwargs={
+                    "action": "remove_targets",
+                    "payload": {**payload, "publish_targets": True},
+                },
+                task_id=fake_task_id,
+                queue="metadata_repository",
+                acks_late=True,
+            )
+        ]
+
 
 class TestPostTargetsPublish:
     def test_post_publish(self, monkeypatch, test_client, token_headers):
-        url = "/api/v1/targets/publish/"
+        url = "/api/v1/artifacts/publish/"
 
         mocked_repository_metadata = pretend.stub(
             apply_async=pretend.call_recorder(lambda **kw: None)
@@ -507,6 +628,56 @@ class TestPostTargetsPublish:
                 "last_update": "2019-06-16T09:05:01",
             },
             "message": "Publish targets successfully submitted.",
+        }
+        assert mocked_repository_metadata.apply_async.calls == [
+            pretend.call(
+                kwargs={
+                    "action": "publish_targets",
+                    "payload": None,
+                },
+                task_id=fake_task_id,
+                queue="rstuf_internals",
+                acks_late=True,
+            )
+        ]
+
+    def test_post_publish_deprecation(
+        self, monkeypatch, test_client, token_headers
+    ):
+        url = "/api/v1/targets/publish/"
+
+        mocked_repository_metadata = pretend.stub(
+            apply_async=pretend.call_recorder(lambda **kw: None)
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.repository_metadata",
+            mocked_repository_metadata,
+        )
+        fake_task_id = uuid4().hex
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.get_task_id",
+            lambda: fake_task_id,
+        )
+        fake_time = datetime.datetime(2019, 6, 16, 9, 5, 1)
+        fake_datetime = pretend.stub(
+            now=pretend.call_recorder(lambda: fake_time)
+        )
+        monkeypatch.setattr(
+            "repository_service_tuf_api.targets.datetime", fake_datetime
+        )
+        response = test_client.post(url, headers=token_headers)
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        assert response.json() == {
+            "data": {
+                "targets": [],
+                "task_id": fake_task_id,
+                "last_update": "2019-06-16T09:05:01",
+            },
+            "message": (
+                "Publish targets successfully submitted. "
+                "Deprecation Warning: this endpoint will removed in v1.0.0, "
+                "use `/api/v1/artifacts`"
+            ),
         }
         assert mocked_repository_metadata.apply_async.calls == [
             pretend.call(
