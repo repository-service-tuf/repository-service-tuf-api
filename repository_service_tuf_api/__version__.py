704fbdc92e6e907b9094c8204b5c385dcd678f1a
Kairo Araujo <kairo@dearaujo.nl>
2024-12-04 20:39:56 +0100

    release: v1.0.0b2 (#737)
    Signed-off-by: Kairo Araujo <kairo.araujo@testifysec.com>

--- repository_service_tuf_api/__version__.py
+++ repository_service_tuf_api/__version__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2023 Repository Service for TUF Contributors
+# SPDX-FileCopyrightText: 2023-2024 Repository Service for TUF Contributors
 # SPDX-FileCopyrightText: 2022-2023 VMware Inc
 #
 # SPDX-License-Identifier: MIT
 
-version = "1.0.0b1"
+version = "1.0.0b2"
 copyright = "Copyright (c) 2024 Repository Service for TUF Contributors"
 author = "Kairo de Araujo"
